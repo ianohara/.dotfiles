3a023559f251839e5e8d66aadff3327b75f645c9
Ian O'Hara <ian.ohara@gmail.com>
2012-07-15 15:01:35 -0400

    Made free-mem.sh for Tmux status bar work on both Mac and Linux

--- .tmux-conf-scripts/free-mem.sh
+++ .tmux-conf-scripts/free-mem.sh
@@ -1,1 +1,8 @@
-top -l 1 | grep PhysMem: | awk '{ print $10  " free / " $8 " used" }'
+if [ `uname` == "Darwin" ]; then
+    TOP_OPT="-l 1"
+elif [ `uname` == "Linux" ]; then
+    TOP_OPT="-n 1"
+fi
+
+
+top $TOP_OPT | grep PhysMem: | awk '{ print $10  " free / " $8 " used" }'

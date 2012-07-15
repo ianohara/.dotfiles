69ba2c08de2be17e9d19b34c02f0536f1c3822ac
Ian O'Hara <ian.ohara@gmail.com>
2012-07-15 14:44:26 -0400

    Again, setup file update from modlab-backup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ read BACKDIR
 
 BACKDIR=`echo "$BACKDIR" | sed 's!/$!!'`
 
-if [ ( -n "$BACKDIR" ) && ( ! -d "$BACKDIR" ) ]; then
+if [ -n "$BACKDIR" ] && [ ! -d "$BACKDIR" ]; then
     echo "The Backup directory specified ($BACKDIR) does not exist."
     exit 1
 fi
+
+DOTBASE=$PWD
 
 # Make $1 a symlink pointing to $2 and backup $1 if the user
 # has already asked us to.
@@ -23,10 +25,10 @@ linkit() {
         return 0
     fi
 
-    if [ ( -e "$1" ) && ( -n "$BACKDIR" ) ]; then
+    if [ -e "$1" ] && [ -n "$BACKDIR" ]; then
         echo "Moving $1 to $BACKDIR/$1.$DATE.bak"
         mv "$1" "$BACKDIR/$1.$DATE.bak"
-    elif [ ( -e "$1" ) && ( -z "$BACKDIR" ) ]; then
+    elif [ -e "$1" ] && [ -z "$BACKDIR" ]; then
         echo "Deleting old $1 in preparation for linking."
         rm $1
     fi
@@ -39,17 +41,17 @@ linkit() {
     ln -s "$2" "$1"
 }
 
-linkit "~/.tmux-conf-scripts" "$PWD/.tmux-conf-scripts/"
-linkit "~/.bash_profile" "$PWD/.bash_profile"
-linkit "~/.vimrc" "$PWD/.vimrc"
-linkit "~/.vim" "$PWD/.vim/"
-linkit "~/.tmux.conf" "$PWD/.tmux.conf"
+linkit "$HOME/.tmux-conf-scripts" "$PWD/.tmux-conf-scripts/"
+linkit "$HOME/.bash_profile" "$PWD/.bash_profile"
+linkit "$HOME/.vimrc" "$PWD/.vimrc"
+linkit "$HOME/.vim" "$PWD/.vim/"
+linkit "$HOME/.tmux.conf" "$PWD/.tmux.conf"
 
 echo  "Cloning the solarized color scheme from altercation's account on github..."
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim-colors-solarized
+git clone git://github.com/altercation/vim-colors-solarized.git $HOME/.vim-colors-solarized
 
 if [ ! -e "$DOTBASE/.vim/bundle/" ]; then
     mkdir "$DOTBASE/.vim/bundle"
 fi
-linkit "$DOTBASE/.vim/bundle/vim-colors-solarized/" "~/.vim-colors-solarized"
+linkit "$DOTBASE/.vim/bundle/vim-colors-solarized" "$HOME/.vim-colors-solarized"
 
